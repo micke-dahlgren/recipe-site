b5aaa4cca51765428c58fbafbd9298656133b10c
Mikael Dahlgren <micke.dahlgren@gmail.com>
2021-05-19 16:15:43 +0200

    showing random recipes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,50 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template >
+<div class="content">
+  <h1>Recipe site</h1>
+  <div class="recipes">
+    <div v-for="el in myArr" :key="el">
+      <Recipe :recipe="el" />
+    </div>
+  </div>
+  <button @click="getData()">GET DATA</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Recipe from './components/Recipe.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Recipe,
+  },
+  data(){
+    return{
+     myArr: []
+    }
+  },
+  methods: {
+    async getData(){
+      let res = await fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=ac54b2c3f7be4b98b26f59ef491867d6');
+      let data = await res.json();
+      console.log(data)
+      this.myArr = data.recipes;
+    }
+  },
+  created(){
+    this.getData();
   }
 }
 </script>
 
 <style>
+  *{
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +52,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+}
+
+.content{
+  width:80vw;
+  margin:auto;
+}
+
+.recipes{
+    width:850px;
+  display:grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap:16px;
 }
 </style>
---
+++ src/components/Recipe.vue
@@ -0,0 +1,28 @@
+<template>
+  <img :src="recipe.image" :alt="recipe.id"/>
+  <h3>{{recipe.title}}</h3>
+  <p v-html="recipe.summary"/>
+</template>
+
+<script>
+export default {
+  name: 'Recipe',
+  props: {
+    recipe: Object,
+  }
+}
+</script>
+
+<style scoped>
+ img{
+   width: 20vw;
+ }
+ p{
+  text-align: left;
+  display: -webkit-box;
+  -webkit-line-clamp: 4; /* number of lines to show */
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+ }
+</style>
